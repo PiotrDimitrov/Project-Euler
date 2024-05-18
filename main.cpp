0582f361a83a4b98cb826d67bf72fc880a5b8c6d
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-18 21:18:01 +0300

    problem 5

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "header.h"
 
 int main() {
-    std::cout << problem4() << std::endl;
+    std::cout << problem5() << std::endl;
     return 0;
 }
---
+++ problems/problem5.cpp
@@ -0,0 +1,33 @@
+/*
+   2520 is the smallest number that can be divided by each of the numbers
+   from 1 to 10 without any remainder.
+
+   What is the smallest positive number that is evenly divisible by all of
+   the numbers from 1 to 20?
+*/
+#include <iostream>
+
+int problem5() {
+    int limit = 20;
+    long long result = 1;
+    int arr[limit];
+    for (int i = 0; i < limit; i++) {
+        arr[i] = 1+i;
+    }
+    for (int i = 1; i < limit; i++){
+        if (arr[i] == 0) { continue;}
+        for (int j = 2*arr[i]; j < limit; j += arr[i]){
+            arr[j-1] = 0;
+        }
+    }
+    for (auto e : arr){
+        if (e < 2) { continue; }
+        int i = 1;
+        while (i*e < limit){
+            i *= e;
+        }
+        std::cout << i << std::endl;
+        result *= i;
+    }
+    return result;
+}
